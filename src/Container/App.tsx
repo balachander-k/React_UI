e6fb662684b3393919c8b5b3344f0b802fea0c5c
balachander-k <chanderbala687@gmail.com>
2025-04-10 09:48:04 +0530

    Initial commit

---
+++ src/Container/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Footer, Header } from '../Components/Layout';
+import { Home } from '../Pages';
+
+
+function App() {
+  
+  
+  return (
+    <div>
+      <Header/>
+      <Home/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Pages/Home.tsx
@@ -0,0 +1,15 @@
+import React  from "react";
+import { MenuItemList } from "../Components/Page/MenuItems";
+
+function Home()
+{
+    return (
+        <div>
+            <div className="container p-2"></div>
+            
+            <MenuItemList/>
+        </div>
+    )
+}
+
+export default Home;
